b410b27afa35147d8b1c9409b2fe5475c9bd5abd
anton-k <anton.kholomiov@gmail.com>
2015-09-02 17:55:36 +0300

    standard for Hm drums

--- src/Csound/Catalog/Drum/Hm.hs
+++ src/Csound/Catalog/Drum/Hm.hs
@@ -11,26 +11,42 @@ module Csound.Catalog.Drum.Hm(
     -- ** Pitched drums
     --
     -- The instrument turns a pitch (in Hz) to the signal.
-    bassDrum, crash, handClap 
+    bassDrum, crash, handClap, 
+    bassDrum', crash', handClap',
+
+    -- * Sampler
+    bd1, bd2, sn1, sweep, boink, sn2, ohh, chh, bd3, cr, clap
 ) where
 
 import Csound.Base
+import Csound.Sam
 
 -- subtractive 
 
 bpb :: Sig -> Sig -> Sig -> Sig
 bpb cfq q asig = balance (bp cfq q asig) asig
 
+rndAmp :: Sig -> SE Sig
+rndAmp a = do
+    k <- birnd 0.09
+    return $ a * (1 + sig k)
+
+addDur' dt x = xtratim dt >> return x
+addDur = addDur' 0.1
+
+toDrum :: Sig -> SE Sig
+toDrum a = rndAmp =<< addDur a
+
 -- | A \"dumb\" drum.
 dumb :: SE Sig
-dumb = fmap (bpb 1000 100) $ rand $ expseg [0.0001, 0.01, 1, 0.04, 0.01]
+dumb = (toDrum =<<) $ fmap (bpb 1000 100) $ rand $ expseg [0.0001, 0.01, 1, 0.04, 0.01]
 
 dumbBass :: SE Sig
-dumbBass = fmap (bpb kfreqenv (kfreqenv / 8)) $ rand $ expseg [0.0001, 0.01, 1, 0.08, 0.01]
+dumbBass = (toDrum =<<) $ fmap (bpb kfreqenv (kfreqenv / 8)) $ rand $ expseg [0.0001, 0.01, 1, 0.08, 0.01]
     where kfreqenv = expseg [50, 0.01, 200, 0.08, 50]
     
-pluckSnare :: Sig 
-pluckSnare = pluck kampenv4 kptchenv 50 (elins [1, 1]) 4 `withDs` [0.8, 3] 
+pluckSnare :: SE Sig 
+pluckSnare = toDrum $ pluck kampenv4 kptchenv 50 (elins [1, 1]) 4 `withDs` [0.8, 3] 
     where
         kampenv4 = linseg [0, 0.001, 1, xdur - 0.21, 1, 0.02, 0]
         kptchenv = linseg [100, 0.01, 300, 0.2, 200, 0.01, 200]
@@ -38,7 +54,7 @@ pluckSnare = pluck kampenv4 kptchenv 50 (elins [1, 1]) 4 `withDs` [0.8, 3]
 
 
 sortaKnockSweep :: SE Sig
-sortaKnockSweep = 
+sortaKnockSweep = (toDrum =<<) $
     fmap (resonsBy bpb
             [ (kfreqenv41, kfreqenv41 / 8)
             , (kfreqenv42, kfreqenv42 / 4) 
@@ -49,16 +65,18 @@ sortaKnockSweep =
         kfreqenv42  = linseg [ 150, 0.01, 1000, 0.08, 250] 
         kampenv4	= linseg [ 0, 0.01, 1, 0.08, 0, 0.01, 0] 
 
-metalBoink :: Sig
-metalBoink = foscil kampenv61 30 1 6.726 kampenv62 sine 
+metalBoink :: SE Sig
+metalBoink = toDrum $ foscil kampenv61 30 1 6.726 kampenv62 sine 
     where
         kampenv61   = expseg [ 0.01, 0.01, 1, 0.2, 0.1, 0.1, 0.001 ]
         kampenv62   = linseg [ 1, 0.1, 10, 0.1, 0.5, 0.01, 1 ]
 
 -- fullkit
 
-bassDrum :: D -> Sig
-bassDrum cps= env * osc (sig cps * kgliss * 0.5)
+bassDrum = bassDrum' 64
+
+bassDrum' :: D -> SE Sig
+bassDrum' cps = toDrum $ env * osc (sig cps * kgliss * 0.5)
     where env = linseg [0, 0.00245, 1, 0.1225, 0]
           kgliss = expseg [10, 0.625, 1, 0.25, 1]  
 
@@ -70,7 +88,7 @@ closedHihat :: SE Sig
 closedHihat = hihat $ linseg [0, 0.00245, 1, 0.1225, 0]
 
 hihat :: Sig -> SE Sig
-hihat kenv = do
+hihat kenv = toDrum =<< do
     asig <- rand kenv
     let a1 = moogvcf (butbp asig cf (cf / 2)) cf 0.7
         a2 = comb a1 0.5 (1/cpspch(8))
@@ -78,7 +96,7 @@ hihat kenv = do
     where cf = cpspch (5.08 + 8)
 
 snare :: SE Sig
-snare = do
+snare = toDrum =<< do
     asound <- rand 1
     let as1 = butbp asound 210 55
     return $ kenv2 * (as1 * 0.9 + asound * 0.8 * kenv1)
@@ -86,9 +104,11 @@ snare = do
         kenv1 = linseg [0, 0.00176, 1, 0.1232, 0]
         kenv2 = expseg [0.01, 0.0002, 1, 0.0297, 0.01, 0.09, 0.01]  
 
+crash = crash' $ cpspch 13.05
+
 -- | Recommended values cpspch(13.03) - cpspch(13.10)
-crash :: D -> SE Sig
-crash cps = do
+crash' :: D -> SE Sig
+crash' cps = toDrum =<< do
     asig <- rand 1
     return $ 0.5 * (0.5 * aall + resonator (aall + asig * kenv2))
     where
@@ -129,8 +149,10 @@ crash cps = do
             , (0.0209, 3.979)
             ]
 
-handClap :: D -> SE Sig
-handClap cps = fmap onNoise $ rand 1
+handClap = handClap' 400
+
+handClap' :: D -> SE Sig
+handClap' cps = (toDrum =<< ) $ fmap onNoise $ rand 1
     where
         kenv1 = expseg [1.25, 0.03, 0.0001]
         kenv2 = expseg [0.001, 0.005, 1, 0.35, 0.001]
@@ -154,3 +176,22 @@ handClap cps = fmap onNoise $ rand 1
 
                 rz x y = resonz x (sig $ cps * y) (sig $ cps * 5.5)
 
+--------------------------------------------------
+-- sampler
+
+mkSam = limSam 1
+
+bd1 = mkSam dumb
+bd2 = mkSam dumbBass
+
+sn1 = mkSam pluckSnare
+sweep = mkSam sortaKnockSweep
+boink = mkSam metalBoink
+
+sn2 = mkSam snare
+ohh = mkSam openHihat
+chh = mkSam closedHihat
+
+bd3 = mkSam bassDrum
+cr  = mkSam crash
+clap = mkSam handClap 
