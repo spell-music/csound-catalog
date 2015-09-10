42b1a53a0bc0cab6a52a33ba17589f2ffddc2692
anton-k <anton.kholomiov@gmail.com>
2015-09-10 14:01:20 +0300

    cfix less-than

--- src/Csound/Catalog/Wave/Amsterdam.hs
+++ src/Csound/Catalog/Wave/Amsterdam.hs
@@ -16,6 +16,6 @@ import Csound.Base
 -- * cps - the frequency of the note
 tibetan :: Int -> Sig -> D -> Sig
 tibetan n off cps = chorusPitch n (2 * off * fromIntegral n) (oscBy wave) (sig cps)
-    where wave = ifB (cps <* 230) (waveBy 5) (ifB (cps `lessThan` 350) (waveBy 3) (waveBy 1))
+    where wave = ifB (cps `lessThan` 230) (waveBy 5) (ifB (cps `lessThan` 350) (waveBy 3) (waveBy 1))
           waveBy x = sines $ [0.3, 0, 0, 0] ++ replicate x 0.1
 
